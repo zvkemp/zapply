f57801c25114b7cb3b8c05008174f91973eca42f
zvkemp <zvkemp@gmail.com>
2013-04-03 18:10:13 -0700

    stubbed home page

---
+++ spec/integration/home_page_test.rb
@@ -0,0 +1,25 @@
+require_relative "../minitest_helper"
+
+describe "The landing page :: integration" do
+  before do
+    visit root_path
+  end
+
+  describe "not logged in" do
+    it "shows the job description" do
+      page.text.must_include "Program Manager"
+    end
+
+    it "shows an apply now button" do
+      page.text.must_include "Apply Now"
+    end
+
+    it "shows a sign in link" do
+      page.text.must_include "sign in"
+    end
+
+    it "shows a sign up link" do
+      page.text.must_include "sign up"
+    end
+  end
+end
