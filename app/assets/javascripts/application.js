2ae033892a8ec503e894235ff18f19575cca1e25
zvkemp <zvkemp@gmail.com>
2013-04-03 18:03:15 -0700

    backbone install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,12 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
+//= require backbone
+//= require zapply_lite
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
