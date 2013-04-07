fb741622b070e66da9653358d7eaab7c70575917
zvkemp <zvkemp@gmail.com>
2013-04-07 15:37:00 -0700

    changed storage directory for uploads

--- app/uploaders/material_uploader.rb
+++ app/uploaders/material_uploader.rb
@@ -2,6 +2,6 @@ class MaterialUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "#{Rails.root}/application_material/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{Rails.root}/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 end
