f66387cef3365ec9171dd89b1f7b0262f813e3fe
zvkemp <zvkemp@gmail.com>
2014-09-15 12:35:25 -0700

    sync

--- app/controllers/api/job_openings_controller.rb
+++ app/controllers/api/job_openings_controller.rb
@@ -1,5 +1,5 @@
 class Api::JobOpeningsController < Api::BaseController
   def show
-    respond_with(@job_opening = JobOpening.last, serializer: JobOpeningSerializer)
+    respond_with(@job_opening = JobOpening.current, serializer: JobOpeningSerializer)
   end
 end
